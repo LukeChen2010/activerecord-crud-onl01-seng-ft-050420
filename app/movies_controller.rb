49b42ad4401961c787c65cf9d13c46e1c40037c6
Luke Chen <luke.chen.2010@gmail.com>
2020-05-26 20:28:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -32,11 +32,11 @@ def can_be_created_in_a_block(args = {"title" => "Home Alone", "release_date" =>
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  return Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  return Movie.last
 end
 
 def can_get_size_of_the_database
